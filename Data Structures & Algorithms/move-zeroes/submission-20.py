1cdd0c00098c418be075578fadd160efb3ff8ba0
Gautam Sonpitale <gautam.sonpitale@gmail.com>
2026-08-31 16:24:34 +0530

    Removed default instructions for compact and clean code.

--- Data Structures & Algorithms/move-zeroes/submission-20.py
+++ Data Structures & Algorithms/move-zeroes/submission-20.py
@@ -1,9 +1,6 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-
+        
         i = 0
         for j in range(len(nums)):
             if nums[j] != 0:
